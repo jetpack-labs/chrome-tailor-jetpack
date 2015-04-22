fcb9030e751bab625aed1fdadc1dd6477449ec3f
Jordan Santell <jsantell@gmail.com>
2015-04-22 00:44:29 -0700

    Add property data to API stub

--- scripts/create-stubs.js
+++ scripts/create-stubs.js
@@ -19,6 +19,7 @@ var output = definitions.reduce(function (output, def) {
   var namespace = output[def.namespace] = {};
   namespace.functions = (def.functions || []).map(createFunctionDefinition);
   namespace.events = (def.events || []).map(createEventDefinition);
+  namespace.properties = createPropertyDefinition(def.properties || {});
   return output;
 }, {});
 
@@ -56,4 +57,38 @@ function createFunctionDefinition (fn) {
 
 function createEventDefinition (ev) {
   return ev.name;
+}
+
+function createPropertyDefinition (props) {
+  if (Object.keys(props).length === 0) {
+    return void 0;
+  }
+
+  return Object.keys(props).reduce(function (result, prop) {
+    var def = props[prop];
+
+    // If it's a reference to another class, we can deal with it later during runtime
+    // to instantiate
+    if (def.$ref) {
+      result[prop] = { class: def.$ref };
+    }
+    // Some properties are just containers to hold more properties
+    else if (def.type === "object" && JSON.stringify(def.value) === "{}") {
+      result[prop] = { container: true };
+    }
+    // Many are just enums or constants with primitives
+    else if (def.value != null && !def.type) {
+      result[prop] = { value: def.value };
+    }
+    // Some are getters that need to be populated during injection, or sometimes dynamically
+    else if (!def.value && def.type) {
+      result[prop] = { getter: true };
+    } else { throw def; }
+
+    if (def.properties && Object.keys(def.properties).length !== 0) {
+      result[prop].properties = createPropertyDefinition(def.properties);
+    }
+
+    return result;
+  }, {});
 }
